b2deed4a03274137b7aedf9d18f9e7cc5f342572
Khanh Nguyen <con.nguyen.86@gmail.com>
2017-08-24 17:00:20 -0700

    completed basic game

--- tetris.js
+++ tetris.js
@@ -3,6 +3,23 @@ const context = canvas.getContext('2d');
 
 context.scale(20,20);
 
+function arenaSweep(){
+    let rowCount = 1;
+    outer: for(let y=arena.length-1;y > 0; --y){
+        for(let x=0; x < arena[y].length; ++x){
+            if (arena[y][x] === 0){
+                continue outer;
+            }
+        }
+        const row = arena.splice(y,1)[0].fill(0)
+        arena.unshift(row);
+        ++y;
+
+        player.score += rowCount * 10;
+        rowCount *=2;
+    }
+}
+
 function collide(arena, player){
     const [m,o] = [player.matrix, player.pos];
     for(let y=0;y < m.length;++y){
@@ -34,39 +51,38 @@ function createPiece(type){
         ];
     }else if(type === 'O'){
         return [
-            [1,1,1],
-            [1,1,1],
-            [1,1,1],
+            [2,2],
+            [2,2],
         ];
     }else if(type === 'L'){
         return [
-            [0,1,0],
-            [0,1,0],
-            [0,1,1],
+            [0,3,0],
+            [0,3,0],
+            [0,3,3],
         ];
     }else if(type === 'J'){
         return [
-            [0,1,0],
-            [0,1,0],
-            [1,1,0],
+            [0,4,0],
+            [0,4,0],
+            [4,4,0],
         ];
     }else if(type === 'I'){
         return [
-            [0,1,0,0],
-            [0,1,0,0],
-            [0,1,0,0],
-            [0,1,0,0],
+            [0,5,0,0],
+            [0,5,0,0],
+            [0,5,0,0],
+            [0,5,0,0],
         ];
     }else if(type === 'S'){
         return [
-            [0,1,1],
-            [1,1,0],
+            [0,6,6],
+            [6,6,0],
             [0,0,0],
         ];
     }else if(type === 'Z'){
         return [
-            [1,1,0],
-            [0,1,1],
+            [7,7,0],
+            [0,7,7],
             [0,0,0],
         ];
     }
@@ -84,7 +100,7 @@ function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if (value !== 0) {
-                context.fillStyle = 'red';
+                context.fillStyle = colors[value];
                 context.fillRect(x + offset.x,
                                 y + offset.y,
                                 1, 1);
@@ -109,6 +125,8 @@ function playerDrop(){
         player.pos.y--;
         merge(arena, player);
         playerReset();
+        arenaSweep();
+        updateScore();
     }
     dropCounter =0;
 }
@@ -128,7 +146,9 @@ function playerReset(){
     player.pos.x = (arena[0].length /2 | 0) -
                     (player.matrix[0].length / 2 | 0);
     if(collide(arena, player)){
-        arena.forEach(row => row.fill(0))
+        arena.forEach(row => row.fill(0));
+        player.score = 0;
+        updateScore();
     }
 }
 
@@ -182,12 +202,28 @@ function update(time = 0){
     requestAnimationFrame(update);
 }
 
+function updateScore(){
+    document.getElementById('score').innerText = player.score;
+}
+
+const colors = [
+    null,
+    '#FF0D72',
+    '#0DC2FF',
+    '#0DFF72',
+    '#F538FF',
+    '#FF8E0D',
+    '#FFE138',
+    '#3877FF',
+]
+
 const arena = createMatrix(12,20);
 // console.log(arena); console.table(arena);
 
 const player = {
-    pos: {x: 5, y: 5},
-    matrix: createPiece('T'),
+    pos: {x: 0, y: 0},
+    matrix: null,
+    score: 0,
 };
 
 document.addEventListener('keydown', event =>{
@@ -203,6 +239,12 @@ document.addEventListener('keydown', event =>{
         playerRotate(1)
     }
 });
-
+playerReset();
+updateScore();
 update();
-//https://www.youtube.com/watch?v=H2aW5V46khA @ 32:31 - creating more pieces
+
+//Need to add UP fast drop and save piece
+//https://www.youtube.com/watch?v=H2aW5V46khA
+
+//Two player
+//https://www.youtube.com/watch?v=JJo5JpbuTTs
